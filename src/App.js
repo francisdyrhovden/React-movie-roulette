629433c2c903982b6a177dfe98c456a55036a043
Francis Soliman <soliman_61@hotmail.com>
2020-01-06 22:29:19 +0100

    Experimented with css:wrench:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './App.css';
+import './desc.css';
 import { Layout, Header, Content, Grid, Cell } from 'react-mdl';
-import Checkbox from '@material-ui/core/Checkbox';
+//import Checkbox from '@material-ui/core/Checkbox';
 
 
 function App() {
     return (
-        <div className="demo-big-content">
+        <div>
             <Layout>
-                <Header className="header-color" title="Movie roulette" scroll>
+                <Header class="header-color" title="Movie roulette" scroll>
                 </Header>
                 <Content>
-                    <div className="landing-body">   
-                        <div className="banner-text">   
-                            <button className="button-css">GO</button>
-                        </div>                               
+                    <div class="landing-body">
+
+                        <div class="css-desc">
+                            <h2>Movie Roulette: What to watch?</h2>
+                            <p>If you're wondering what movie or TV show you
+                                should watch next, use the randomizer below.</p>
+                        </div>
+
+                        <div class="css-box">
+                            <div class="css-left">
+                                <button class="button-css">GO</button>
+                            </div>
+                            <div class="css-right">
+                                <div class="css-result">
+                                    <p>Let fate take the wheel... Have a spin.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Content>
             </Layout>
